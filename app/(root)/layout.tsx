ae69a11a4d2083a37633103aa6ecb430b6af7d7d
Mrpriven <bekirhabib1@gmail.com>
2025-02-09 00:12:59 +0100

    add initial components and layout for banking application

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,5 @@
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -5,7 +7,7 @@ export default function RootLayout({
 }>) {
   return (
     <main>
-        SIDEBAR
+        
         {children}
     </main>
   );
