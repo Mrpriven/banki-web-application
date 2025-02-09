e7e73f138f599932e5b938da2f035bb7f6daa259
Mrpriven <bekirhabib1@gmail.com>
2025-02-09 11:22:25 +0100

    implement auth, appwrite and sentry

---
+++ sentry.client.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://7093cb4fbeda60177d9e2b229d155c53@o4508788668235777.ingest.de.sentry.io/4508788671709264",
+
+  integrations: [
+    Sentry.replayIntegration(),
+  ],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
